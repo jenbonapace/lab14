d2778e026db12105ef79e5acb2aa9d738866d6b4
Jennifer Bonapace <jennifer.bonapace@gmail.com>
2017-02-22 14:19:39 -0500

    :

--- lab14controllera.js
+++ lab14controllera.js
@@ -1,27 +1,3 @@
 var app = angular.module('Module');
-
-
-app.controller('lab14controllera', function()
-// {   // cool controller stuff
-{.when('/dog', {
-  controller:"lab14controllera",
-  templateUrl:"dog.html"
-})
-.when("/cat", {
-  controller: "lab14controllera",
-  templateUrl: "cat.html"
-})
-.when("/fish", {
-  controller: "lab14controllera",
-  templateUrl: "fish.html"
-})
-.when("/horse", {
-controller: "lab14controllera",
-templateUrl: "horse.html"
-})
-.otherwise({redirectTo:"indexlab14.html"});
-$locationProvider.hashPrefix('');
-});
-
-
+app.controller('lab14controllera', function(){
 });
