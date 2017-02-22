bd9e48ada53a8eba8890421e326d50559dd71a82
Jennifer Bonapace <jennifer.bonapace@gmail.com>
2017-02-22 06:41:10 -0500

    first

---
+++ lab14.js
@@ -0,0 +1,21 @@
+var app = angular.module('Module', ['ngRoute']);
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider .when('/dog', {
+    controller:"lab14controllera",
+    templateUrl:"dog.html"
+  })
+  .when("/cat", {
+    controller: "lab14controllera",
+    templateUrl: "cat.html"
+  })
+  .when("/fish", {
+    controller: "lab14controllera",
+    templateUrl: "fish.html"
+})
+.when("/horse", {
+  controller: "lab14controllera",
+  templateUrl: "horse.html"
+})
+.otherwise({redirectTo:"indexlab14.html"});
+$locationProvider.hashPrefix('');
+});
